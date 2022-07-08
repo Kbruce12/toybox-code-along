f1678ce237fd43ed6c936eefd44e2a2b8f9f9b3d
KevinBruce <kevinlawbruce@gmail.com>
2022-07-08 14:57:17 -0400

    Added JS functionality to slide out nav

---
+++ scripts.js
@@ -0,0 +1,27 @@
+// Pseudo Code
+// Create a functional nav-menu pop out
+// create variables that select the menu button as well as the 'X' while the pop out nav is open
+// Create a variable to target the entire slide out nav
+// Toggle its class to either show or hide
+// Add eventlisteners('click') to both the menu button as well as the 'x'
+
+
+const openButton = document.querySelector('.menuButton img');
+console.log(openButton);
+const closeButton = document.querySelector('.menuClose');
+console.log(closeButton);
+const slideOutNav = document.querySelector('div .slideOutNav');
+console.log(slideOutNav);
+
+openButton.addEventListener('click', function(){
+    console.log('whats up');
+    slideOutNav.classList.toggle('openSlideBar')
+})
+
+closeButton.addEventListener('click', function(){
+console.log('hit me again');
+    slideOutNav.classList.toggle('openSlideBar');
+})
+
+
+
